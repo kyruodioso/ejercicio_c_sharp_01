4750d6c84cbe244f8fad8571c75f8108640ba4df
kyru odioso <kanu_nirvana@hotmail.com>
2022-06-26 12:37:00 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿/*Ejercicio 1
+
+Escribe un programa que reciba tu nombre y lo escriba por consola.*/
+
+string nombre;
+
+Console.WriteLine("Ingresa tu nombre");
+nombre= Console.ReadLine();
+Console.WriteLine("tu nombre es " + nombre);
